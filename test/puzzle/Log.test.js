9f6bcbbd6ba9e5dcdb126b6cd5881ca8b07e078f
Valentin Duricu <valentin.duricu@spark-dev.ro>
2018-05-02 20:27:24 +0300

    Fix failing test

--- test/puzzle/Log.test.js
+++ test/puzzle/Log.test.js
@@ -3,8 +3,18 @@
 const {expect} = require("chai");
 
 const Log = require("../../src/puzzle/Log");
+let puzzle = null;
 
 describe("Log class check", () => {
+  let cwd = '';
+  before(() => {
+    cwd = process.cwd();
+    process.chdir(`${__dirname}/../_toolkit/starter`);
+    puzzle = require("../../src/puzzleInit");
+  });
+  after(() => {
+    process.chdir(cwd);
+  });
   it("className should be Log", () => {
     const pobj = new Log();
     expect(pobj.className).to.be.a("string");
