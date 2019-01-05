fc3d3dc5b4a64b7361bb11a69177ec90b4d40ffb
Valentin Duricu <valentin.duricu@spark-dev.ro>
2019-01-05 23:02:03 +0200

    Command initialization and loading name directly from the command. Load commands

--- src/cli/CommandLoader.js
+++ src/cli/CommandLoader.js
@@ -2,7 +2,10 @@
 
 /** global: puzzle */
 
+const _ = require("lodash");
 const cli = require("cli");
+const fs = require("fs");
+const path = require("path");
 
 const PUse = require("../core/PUse");
 const CLIBase = require("./CLIBase");
@@ -46,16 +49,48 @@ class CommandLoader extends PUse {
   /**
    * Register commands that can be called by the CLI Framework.
    *
-   * @param {string} commandName The name of the command.
+   * @param {*} commandName The name of the command.
    * @param {*} commandInstance The object that performs the actions for the given command.
    *
    * @throws InvalidInstanceType
    */
-  register(commandName, commandInstance) {
+  register(commandName, commandInstance = null) {
+    if (_.isObject(commandName)) {
+      commandInstance = commandName;
+      commandName = "";
+    }
+
     if (!(commandInstance.prototype instanceof CLIBase)) {
       throw new InvalidInstanceType("CLIBase");
     }
+
+    if (commandName === "") {
+      commandName = (new commandInstance()).name;
+    }
+
     this._commands[commandName] = commandInstance;
+  }
+
+  /**
+   * Register an API routes folder into the engine.
+   *
+   * @param {string} root The root folder of the module.
+   * @param {string} [routes=routes] The name of the routes folder.
+   */
+  load(root, routes = "commands") {
+    const fullPath = path.join(root, routes);
+    fs.readdirSync(fullPath).forEach((file) => {
+      if (path.extname(file) !== ".js") {
+        return;
+      }
+
+      const ClassPath = require(path.resolve(fullPath, file));
+      if (!this.isValid(ClassPath) || !_.isFunction(ClassPath)) {
+        return;
+      }
+
+      this.register(ClassPath);
+    });
   }
 
   /**
@@ -65,26 +100,25 @@ class CommandLoader extends PUse {
     puzzle.cli.setApp(puzzle.app.name, puzzle.app.version);
 
     puzzle.cli.parse(
-      {
-        usage: [false, "Display help and usage information"]
-      },
+      null,
       this.isValid(this._commands) ? Object.keys(this._commands) : []
     );
     const { command } = puzzle.cli;
     const isUsage = !!puzzle.cli.options.usage;
 
-    if (!this.isValid(this._commands)
-      || Object.keys(this._commands)
-        .indexOf(command) < 0) {
-      puzzle.cli.getUsage(0);
-      if (!isUsage) {
-        puzzle.cli.fatal("Unable to find the given command!");
-      }
-      return;
+    if (!this.isValid(this._commands)) {
+      puzzle.cli.fatal("Unable to find the given command!");
     }
 
     const commandInst = new this._commands[command]();
-    puzzle.cli.parse(commandInst.options);
+    puzzle.cli.parse(
+      Object.assign(
+        {
+          usage: [false, "Display help and usage information"]
+        },
+        commandInst.options
+      )
+    );
     const { options, args } = puzzle.cli;
 
     if (isUsage) {
